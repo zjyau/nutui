9c91fb21b86d072f3fc51d1c249179eda18790c0
richard1015 <51844712@qq.com>
2020-01-14 20:02:31 +0800

    fix: modify cli index command desc

--- lib/plugin/cli/src/bin/index.ts
+++ lib/plugin/cli/src/bin/index.ts
@@ -8,40 +8,43 @@ import { createComponent } from '../commands/createComponent';
 import { commitLint } from '../commands/commitLint';
 import { test } from '../commands/test';
 import { release } from '../commands/npmPublish';
+import { ROOT_CLI_PATH } from '../common/dic';
 
-program
-    .version(`@nut/cli 1.0`, '-v', '--version')
-    .command('dev')
+const config = require(ROOT_CLI_PATH('package.json'));
+program.version(`@nut/cli ${config.version}`, '-v', '--version')
+
+program.command('dev')
     .description('本地调试运行官网和Demo示例')
     .action(dev)
 
-    .command('build')
+program.command('build')
     .description('构建完整版nutui和各个组件可发布到npm的静态资源包')
     .action(build)
 
-    .command('build-site')
+program.command('build-site')
     .description('构建官网和Demo示例，进行官网发布')
     .action(buildSite)
 
-    .command('create')
+program.command('create')
     .description('新增组件使用该命令')
     .action(createComponent)
 
-    .command('commit-lint')
+program.command('commit-lint')
     .description('git commit 提交校验配置文件')
     .action(commitLint)
 
-    .command('test')
+program.command('test')
     .description('运行单元测试')
     .action(test)
 
-    .command('lint')
+program.command('lint')
     .description('运行stylelint和eslint')
     .action(test)
 
-    .command('release')
+program.command('release')
     .description('发布版本...待开发')
     .action(release)
+    
 program.parse(process.argv);
 
 
