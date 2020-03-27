8d28b4585bf5157da156d206d61693701979edbc
张宇 <jerry@zhangyudeMacBook-Pro.local>
2020-03-27 11:00:00 +0800

    feat: composition-api封装

---
+++ src/utils/composition-api.js
@@ -0,0 +1,59 @@
+import {
+	createComponent,
+	defineComponent,
+	ref,
+	reactive,
+	watch,
+	onMounted,
+	onUpdated,
+	onUnmounted,
+	computed,
+	isRef,
+	toRefs,
+	provide,
+	inject,
+	onActivated,
+	onDeactivated,
+	onBeforeMount,
+	onBeforeUnmount,
+	onBeforeUpdate,
+	onErrorCaptured,
+} from '@vue/composition-api';
+
+export const nCreateComponent = createComponent;
+
+export const nDefineComponent = defineComponent;
+
+export const nRef = ref;
+
+export const nReactive = reactive;
+
+export const nWatch = watch;
+
+export const nComputed = computed;
+
+export const nIsRef = isRef;
+
+export const nToRefs = toRefs;
+
+export const nProvide = provide;
+
+export const nInject = inject;
+
+export const nOnMounted = onMounted;
+
+export const nOnUpdated = onUpdated;
+
+export const nOnUnmounted = onUnmounted;
+
+export const nOnActivated = onActivated;
+
+export const nOnDeactivated = onDeactivated;
+
+export const nOnBeforeMount = onBeforeMount;
+
+export const nOnBeforeUnmount = onBeforeUnmount;
+
+export const nOnBeforeUpdate = onBeforeUpdate;
+
+export const nOnErrorCaptured = onErrorCaptured;
